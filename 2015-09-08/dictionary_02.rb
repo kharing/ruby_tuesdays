9ef631d4c2b129e3df14bc36c252849f79553105
Raphaela Wrede <raphaela.wrede@gmail.com>
2015-09-15 09:05:45 +0200

    add dictionary solution for interating through the whole dictionary

---
+++ 2015-09-08/dictionary_02.rb
@@ -0,0 +1,21 @@
+dictionary = {
+  'milk'   => 'Milch',
+  'coffee' => 'Kaffee',
+  'sugar'  => 'Zucker'
+}
+
+dictionary.each do |word, translation|
+
+  puts "Please enter the German translation for #{word}:"
+
+  entered = gets.strip
+
+  if entered == translation
+    puts "Your answer is correct."
+  else
+    puts "Your answer is not correct. The German translation for #{word} is #{translation}."
+  end
+end
+
+
+
