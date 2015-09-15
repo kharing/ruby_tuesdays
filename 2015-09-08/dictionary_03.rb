6a5a6352333ab8100bb0f63d8672505fbe0cd3a9
Raphaela Wrede <raphaela.wrede@gmail.com>
2015-09-15 09:13:19 +0200

    add solution for number of correct answers

---
+++ 2015-09-08/dictionary_03.rb
@@ -0,0 +1,26 @@
+dictionary = {
+  'milk'   => 'Milch',
+  'coffee' => 'Kaffee',
+  'sugar'  => 'Zucker'
+}
+
+correct_answers = 0
+
+dictionary.each do |word, translation|
+
+  puts "Please enter the German translation for #{word}:"
+
+  entered = gets.strip
+
+  if entered == translation
+    puts "Your answer is correct."
+    correct_answers = correct_answers + 1
+  else
+    puts "Your answer is not correct. The German translation for #{word} is #{translation}."
+  end
+end
+
+puts "You answered #{correct_answers} of #{dictionary.size} questions correctly."
+
+
+
