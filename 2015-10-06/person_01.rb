77fb41789bfbeae3971b8d4e1135f2550c5374ba
Raphaela Wrede <raphaela.wrede@gmail.com>
2015-10-08 14:59:52 +0200

    add solution for Exercise #1

---
+++ 2015-10-06/person_01.rb
@@ -0,0 +1,15 @@
+class Person
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+end
+
+person = Person.new("Angela", "Merkel")
+puts person.full_name
